4f65d82d6dc020d6482a664c181d277764048138
한결같음 <83993373+ocxh@users.noreply.github.com>
2022-02-26 15:11:04 +0900

    Example: MUL

---
+++ mul.asm
@@ -0,0 +1,19 @@
+%include "io64.inc"
+
+section .text
+global CMAIN
+CMAIN:
+    ;write your code here
+    PRINT_DEC 1,ax
+    NEWLINE
+    
+    mov ax,0
+    mov al,2
+    mov bl,3
+    mul bx
+    
+    PRINT_DEC 1,ax
+    NEWLINE
+    
+    xor rax, rax
+    ret
